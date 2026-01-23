1df84db4b0c48fa0a5b17d22c6e5c1c16540f491
Joao Pedro <joaosao291@gmail.com>
2026-01-23 01:04:04 -0300

    inicio do desenvolvimento do front-end
    Implementa consumo da API com operações GET e POST no Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,41 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ProdutoService, Produto } from './services/produto.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'controle-estoque-front';
+export class AppComponent implements OnInit {
+  produtos: Produto[] = [];
+
+  novoProduto = {
+    nome: '',
+    quantidade: 0,
+    preco: 0,
+  };
+
+  constructor(private produtoService: ProdutoService) {}
+
+  ngOnInit() {
+    this.carregarProdutos();
+  }
+
+  carregarProdutos() {
+    this.produtoService.getProdutos().subscribe((dados) => {
+      this.produtos = dados;
+    });
+  }
+
+  criarProduto() {
+    this.produtoService.criarProduto(this.novoProduto).subscribe({
+      next: () => {
+        this.carregarProdutos();
+        this.novoProduto = { nome: '', quantidade: 0, preco: 0 };
+      },
+      error: (err) => alert(err.error),
+    });
+  }
 }
---
+++ src/app/services/produto.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export interface Produto {
+  id: number;
+  nome: string;
+  quantidade: number;
+  preco: number;
+  estoqueBaixo: boolean;
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProdutoService {
+  private apiUrl = 'https://localhost:7174/api/produto';
+
+  constructor(private http: HttpClient) {}
+
+  getProdutos(): Observable<Produto[]> {
+    return this.http.get<Produto[]>(this.apiUrl);
+  }
+
+  criarProduto(produto: any) {
+    return this.http.post(this.apiUrl, produto);
+  }
+}
